e86bb01c86fcf03aaabe3bf6fb7a71501be0521d
vladok089 <vladok.sha089@gmail.com>
2020-09-09 20:09:41 +0300

    gh pages upload

---
+++ precache-manifest.dbfd8f25b807ada9ebcd673377edf33f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f36f64d336d6e4b7fd0cd8e0afc5d12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13e0ac67a4359db2163d",
+    "url": "/static/css/main.cd9e8610.chunk.css"
+  },
+  {
+    "revision": "f9d08e6beb4078008728",
+    "url": "/static/js/2.f3cb8cbe.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.f3cb8cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e0ac67a4359db2163d",
+    "url": "/static/js/main.5e9e0f2a.chunk.js"
+  },
+  {
+    "revision": "370c660593238d16344c",
+    "url": "/static/js/runtime-main.180c528d.js"
+  }
+]);
