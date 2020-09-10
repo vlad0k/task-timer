fb119f931baf04a246c77d167c5266d442b581c5
vladok089 <vladok.sha089@gmail.com>
2020-09-10 16:40:30 +0300

    Updates

---
+++ precache-manifest.2226488c344842f7a34f48654481fe33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5303c1783a251760def840d6c2ee1bba",
+    "url": "/task-timer/index.html"
+  },
+  {
+    "revision": "d5c9257d01e828e36f7d",
+    "url": "/task-timer/static/css/main.475a4db2.chunk.css"
+  },
+  {
+    "revision": "f9d08e6beb4078008728",
+    "url": "/task-timer/static/js/2.f3cb8cbe.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/task-timer/static/js/2.f3cb8cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c9257d01e828e36f7d",
+    "url": "/task-timer/static/js/main.8ed4faa6.chunk.js"
+  },
+  {
+    "revision": "ca42757cdb1e8ef0c6aa",
+    "url": "/task-timer/static/js/runtime-main.aa04110a.js"
+  }
+]);
